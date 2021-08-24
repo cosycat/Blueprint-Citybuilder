d7cab349489661ebe4eb4b2bbc9107a0cd36a9f9
Flavia <flavia.brogle@students.fhnw.ch>
2021-08-24 22:39:49 +0200

    Start working on inhabitants

--- Assets/Scripts/Structure.cs
+++ Assets/Scripts/Structure.cs
@@ -1,16 +1,46 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 
 public class Structure : MonoBehaviour
 {
+    #region Type Infos
+
     private StructureType _type;
 
     public string Name => _type.name;
     public Sprite Sprite => _type.sprite;
     public int TotalHealth => _type.totalHealth;
+    public int MaxInhabitants => _type.maxInhabitants;
+    public bool IsHabitable => _type.maxInhabitants > 0;
+
+    #endregion
+
+
+    #region Instance Infos
 
     public int CurrentHealth { get; private set; }
+
+    #endregion
+
+    
+    #region Housing
+
+    private readonly List<Inhabitant> _inhabitants = new List<Inhabitant>();
+
+    public int CurrentInhabitantCount => _inhabitants.Count;
+    public int FreeHabitableSpace => MaxInhabitants - CurrentInhabitantCount;
+    
+    public void AddInhabitant(Inhabitant newInhabitant)
+    {
+        if (CurrentInhabitantCount >= MaxInhabitants)
+            throw new ArgumentOutOfRangeException(
+                $"A house can't have more inhabitants than it's MaxInhabitants value! newInhabitant: {newInhabitant}, maxInhabitants: {MaxInhabitants}, previous amount of habitants: {CurrentInhabitantCount}");
+        _inhabitants.Add(newInhabitant);
+    }
+
+    #endregion
 
     public void Setup(StructureType type)
     {
@@ -18,12 +48,11 @@ public class Structure : MonoBehaviour
         {
             throw new Exception("Structure Type already set!");
         }
+
         _type = type;
         CurrentHealth = TotalHealth;
 
         var spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
         spriteRenderer.sprite = Sprite;
     }
-    
 }
-
--- Assets/Scripts/StructureType.cs
+++ Assets/Scripts/StructureType.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 [Serializable]
 public class StructureType
-{ 
+{
     // TODO make the fields readonly once they are read in from a file.
+    // TODO once we read them in from a file, we can let them inherit from each other.
     [SerializeField] public string name;
     [SerializeField] public Sprite sprite;
     [SerializeField] public int totalHealth = 100;
     [SerializeField] public BuildMode buildMode = BuildMode.Single;
-
+    [SerializeField] public int maxInhabitants = 0;
 
 }
 
